872c11659c7a9ce47cb75b246b9d7dd8298c92d8
Anugraha2526 <1234567890anugraha@gmail.com>
2024-04-02 16:20:23 +0545

    printer website

---
+++ Javascript/myscript.js
@@ -0,0 +1,13 @@
+function addcart()
+{
+    alert("Added to cart successfully");
+}
+       
+function cart()
+{
+    alert("Thank you for using our cart");
+}
+
+function search(){
+    alert("This feature is comming soon");
+}
